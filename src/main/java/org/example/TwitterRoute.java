9b2711568b676ca729c14636ebca94b3d7d084c0
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2021-05-07 14:38:02 +0200

    right

--- src/main/java/org/example/TwitterRoute.java
+++ src/main/java/org/example/TwitterRoute.java
@@ -6,10 +6,16 @@ import org.apache.camel.builder.RouteBuilder;
 
 @ApplicationScoped
 public class TwitterRoute extends RouteBuilder {
+
     @Override
     public void configure() throws Exception {
-        from("twitter-search://@Apachecamel?count=100")
-                .log("log:${body}")
-                .to("kafka:sighting");
+
+        // example of Routes that need the class Account to be registred for reflection
+        from("twitter-search://@Apachecamel")
+                .setBody()
+                .simple("{\"sigthning\": \"${body}\", \"where\": \"twitter\"}")
+                .marshal().json()
+                .log("${body}")
+                .to("kafka:camelsightings");
     }
 }
